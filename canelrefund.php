873c3689ec6429ea5bfe6ce85979c9479041b043
racchanak <racchana.appiness@gmail.com>
2017-11-06 18:54:37 +0530

    changed

---
+++ canelrefund.php
@@ -0,0 +1,41 @@
+<?php
+	include('functioncall.php');
+	$title = 'Shipping and Delivery Policy';
+	$menu = 'terms';
+	include('header.php');
+?>	
+	<section>
+		<div class="container">
+			<div class="row">				
+				<?php include('category.php'); ?>
+				<div class="col-sm-9">
+					<div class="blog-post-area">
+						<h2 class="title text-center">Cancellation and Return & Refund Policy</h2>
+						<div class="single-blog-post">							
+							<p><strong>Cancellation Policy :</strong></p><br>
+							<p>You can cancel your order through online before the product has been shipped. If You want to cancel your order simply send email to care@nattupuram.com or call us @ 8884442552. Your entire order amount will be refunded.</p> <br>
+							<p>Unluckily, an order cannot be cancelled once the product has been dispatched to you. If you still wish to cancel your order after having received the product, refund can only be done if the product is unopened, undamaged and returned back to Us in the same condition as it was received in with original packaging. Keep in mind however, that the shipping costs have to be borne by the customer. The refund shall be processed only once after we receive the order back and examining its condition. Please allow approximately 2 weeks for your refund to be processed from the date of receiving back the goods.</p><br>
+							<p>We have all rights to cancel your order in case of insufficient stocks or in case of any suspicious order transactions. Auto cancellation may also be triggered by the administration end in case of any improper order placement (like incomplete address) and/or due to any technical difficulties.<p></br>
+							<p>We reserve the sole right to cancel your order in the following circumstance:</p>
+							<ul>
+								<li>In the event of the designated address following outside the delivery zone offered by us.</li>
+								<li>Failure to contact you by phone or email at the time of confirming the order booking.</li>
+								<li>Failure to deliver your order due to lack of information, direction or authorization from you at the time of delivery.</li>
+								<li>Availability of all the items ordered by you at the time of booking the order.</li>
+							</ul>
+							<p><strong>Return & Refund Policy:</strong></p>
+							<p>Your complete satisfaction with our product is guaranteed or items may be returned or exchanged within 5 days of receipt. Please note that to be eligible for return, items must be in their original purchase condition and shipped within 5 days from the date of receipt. Send us back the complete package with receipt or proof of purchase and please mentioned the order number on the packet.</p>
+							<p><strong>In order to return your product, you should courier your product to:</strong></p>
+							<p>Redibro Enterprises, No:7/26, Reddiyar Street, Reddichavadi, Cuddalore-607402, Tamil Nadu, India.</p>
+							<p>The refund shall be processed only once after we receive the order back and examining its condition. Please allow approximately 2 weeks for your refund to be processed from the date of receiving back the goods. Also send us an email of your returning product with the order number mentioned, tracking number, name of courier services through which the refund procedure has been done and also mentioning your bank details to care@nattupuram.com. We shall credit the amount back to your account.</p>
+							<p>You will be responsible for paying for your own shipping costs for returning your item. Shipping costs are non-refundable. If you receive a refund, the cost of shipping will be deducted from your refund.</p>
+							<p>The only thing that you should keep in mind is that only those goods can be returned and refunded which are not damaged and the packaging is kept intact. Our policy lasts 5 days. If 5 days have gone by since your purchase, unluckily we can’t offer you a refund or exchange.</p>
+						</div><!--/blog-post-area-->
+					</div><!--/Repaly Box-->
+				</div>	
+			</div>
+		</div>
+	</section>	
+<?php
+	include('footer.php');
+?>
---
+++ shippingpolicy.php
@@ -0,0 +1,61 @@
+<?php
+	include('functioncall.php');
+	$title = 'Shipping and Delivery Policy';
+	$menu = 'terms';
+	include('header.php');
+?>	
+	<section>
+		<div class="container">
+			<div class="row">				
+				<?php include('category.php'); ?>
+				<div class="col-sm-9">
+					<div class="blog-post-area">
+						<h2 class="title text-center">Shipping and Delivery Policy</h2>
+						<div class="single-blog-post">
+							<h2>Shipping Mode: Standard Shipping - Surface By Road</h2>
+							<p>All products are either delivered by Nattupuram staff or by reputed courier Companies. Normally for orders placed before 12 PM on a business day, we aim to ship the same day, provided that the card security checks are complete, payment is received and the stock availability is confirmed. Orders placed after 12 PM will be shipped the next business day. Orders Received on Sunday or during Holidays are dispatched the following Monday or the next working day. During busy times, such as holiday periods, there can be processing and shipping delays. The orders cannot be shipped to PO boxes or military addresses, and rural domestic addresses require one or more additional days to deliver. If your location is not serviced by our courier, we will contact you for other possibilities or to cancel the order and full amount will be refunded to your account. If in any case you receive a package which is damaged during transit then please do not accept it. You can return the same and contact us asking for another piece. Please follow the same in case you've received an incorrect product and we would accept it only if you wouldn't open the packaging. We will try our level best to send you a replacement as soon as possible.</p> <br>
+							<p><table class="nfacts">
+                                <thead>
+                                    <tr>
+                                        <th>S.No</th>
+                                        <th>Destination (City/State)</th>
+                                        <th>Delivery Duration (approx.)</th>
+                                        <th>Shipping Charges</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Chennai & Bengaluru</td>
+                                        <td>1-2 Days</td>
+                                        <td>Free Delivery</td>
+                                    </tr>
+                                    <tr>
+                                        <td>2</td>
+                                        <td>Tamil Nadu & Puducherry</td>
+                                        <td>2-3 Days</td>
+                                        <td>Free Delivery</td>
+                                    </tr>
+                                    <tr>
+                                        <td>3</td>
+                                        <td>Kerala, Karnataka, Telangana & AP</td>
+                                        <td>3-5 Days</td>
+                                        <td>Rs. 75 per Litre</td>
+                                    </tr>
+                                    <tr>
+                                        <td>4</td>
+                                        <td>Other Locations</td>
+                                        <td>9-10 Days</td>
+                                        <td>Rs. 100 per Litre</td>
+                                    </tr>
+                                </tbody>
+                            </table></p> <br>
+						</div><!--/blog-post-area-->
+					</div><!--/Repaly Box-->
+				</div>	
+			</div>
+		</div>
+	</section>	
+<?php
+	include('footer.php');
+?>
