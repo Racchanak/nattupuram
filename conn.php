94c9a8b8d34430d3d9454827747c982dfe23b372
racchanak <racchana.appiness@gmail.com>
2017-11-07 19:10:41 +0530

    Contant Changes

--- conn.php
+++ conn.php
@@ -1,4 +1,4 @@
 <?php
-	// $GLOBALS['link'] = mysqli_connect('localhost','root','root','nattupuram') or die('Error Connecting to PHPmyadmin');
-	$GLOBALS['link'] = mysqli_connect('50.62.209.123:3306','nattupuram','nATTUPURAMpALsUJI3','nattupuram') or die('Error Connecting to PHPmyadmin');
+	$GLOBALS['link'] = mysqli_connect('localhost','root','root','nattupuram') or die('Error Connecting to PHPmyadmin');
+	// $GLOBALS['link'] = mysqli_connect('50.62.209.123:3306','nattupuram','nATTUPURAMpALsUJI3','nattupuram') or die('Error Connecting to PHPmyadmin');
 ?>
