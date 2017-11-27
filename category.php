b1c0b5eab26624f43ecc33ef62cd908ff24667f8
racchanak <racchana.appiness@gmail.com>
2017-11-27 18:44:50 +0530

    all neww changes with the functions and the gallery

--- category.php
+++ category.php
@@ -5,15 +5,15 @@
         <div class="panel-group category-products" id="accordian"><!--category-productsr-->
             <div class="owl-carousel owl-theme">
                 <div class="item"><img src="assets/images/gallery/1.jpg" alt="" style="height: 350px;"/></div>
-                <div class="item"><img src="assets/images/gallery/2.jpg" alt=""  style="height: 350px;"/></div>
-                <div class="item"><img src="assets/images/gallery/3.jpg" alt=""  style="height: 350px;"/></div>
-                <div class="item"><img src="assets/images/gallery/4.jpg" alt=""  style="height: 350px;"/></div>
-                <div class="item"><img src="assets/images/gallery/5.jpg" alt=""  style="height: 350px;"/></div>
-                <div class="item"><img src="assets/images/gallery/6.jpg" alt=""  style="height: 350px;"/></div>
-                <div class="item"><img src="assets/images/gallery/7.jpg" alt=""  style="height: 350px;"/></div>
-                <div class="item"><img src="assets/images/gallery/8.jpg" alt=""  style="height: 350px;"/></div>
-                <div class="item"><img src="assets/images/gallery/9.jpg" alt=""  style="height: 350px;"/></div>
-                <div class="item"><img src="assets/images/gallery/10.jpg" alt=""  style="height: 350px;"/></div>
+                <div class="item"><img src="assets/images/gallery/2.jpg" alt="" style="height: 350px;"/></div>
+                <div class="item"><img src="assets/images/gallery/3.jpg" alt="" style="height: 350px;"/></div>
+                <div class="item"><img src="assets/images/gallery/4.jpg" alt="" style="height: 350px;"/></div>
+                <div class="item"><img src="assets/images/gallery/5.jpg" alt="" style="height: 350px;"/></div>
+                <div class="item"><img src="assets/images/gallery/6.jpg" alt="" style="height: 350px;"/></div>
+                <div class="item"><img src="assets/images/gallery/7.jpg" alt="" style="height: 350px;"/></div>
+                <div class="item"><img src="assets/images/gallery/8.jpg" alt="" style="height: 350px;"/></div>
+                <div class="item"><img src="assets/images/gallery/9.jpg" alt="" style="height: 350px;"/></div>
+                <div class="item"><img src="assets/images/gallery/10.jpg" alt="" style="height: 350px;"/></div>
                 <!-- <div class="item"><h4>11</h4></div>
                 <div class="item"><h4>12</h4></div> -->
             </div>
--- functioncall.php
+++ functioncall.php
@@ -1,45 +1,56 @@
-<?php include('function.php');
- $product_reviews = reviews_data(); 
- $products = products_data();
- $offers = offers_data('');
- // randomString(3);
- $product_id = (isset($_REQUEST['product_id']))?$_REQUEST['product_id']:'';
- $product = products_data($product_id);
- $productDiscount = array();
- foreach ($product[0]['price'] as $pkey => $pkvalue) {
- 	$pprice = $pkvalue - ($pkvalue*$offers['Welcome'][0]['Offersvalue'])/100;
- 	array_push($productDiscount, $pprice);
- }
- $product[0]['discount'] = $productDiscount;
- if(isset($_SESSION['user']['register_id'])) {
- 	$user_id = $_SESSION['user']['register_id'];
- 	$product_cart = carts_data($user_id); 
- 	foreach ($product_cart as $key => $value) {
- 		$orders = order_data($user_id,$value['cart_id']);
- 		if(!empty($orders)) {
- 			$product_order[$key] = $orders[0];
- 			$product_details = products_data($orders[0]['product_id']);
- 			$product_order[$key]['quantity'] = $value['quantity'];
- 			$product_order[$key]['price'] = $value['price'];
- 			$product_order[$key]['product_name'] = $value['product_name'];
- 			$product_order[$key]['products_offers'] = $product_details[0]['offers'];
- 		}
- 	}
- } else if((isset($_COOKIE['Guest_cart'])) && ($_COOKIE['Guest_cart'] > 0)) {
- 	$order_id = $_COOKIE['Guest_cart'];
- 	$orders = order_data('','',$order_id);
-	if(!empty($orders)) {
-		$product_order[0] = $orders[0];
-		$product_details = products_data($orders[0]['product_id']);
-		if(!empty($product_details[0]['offer_ids'])) {
-			$product_offers = explode(',', $product_details[0]['offers']);
-			$offers_id = explode(',', $product_details[0]['offer_ids']);
-	 		foreach ($offers_id as $key => $value) {
-				$offer_details = offers_data($value);
-				$product_order[0]['offers'][$key] = $product_offers[$key];
-	 			$product_order[0]['products_offers'][$key] = $offer_details[0];
-	 		}
- 		}
-	}
- }
- ?>
+<?php
+
+include('function.php');
+$product_reviews = reviews_data();
+$products = products_data();
+$offers = offers_data('');
+// randomString(3);
+$product_id = (isset($_REQUEST['product_id'])) ? $_REQUEST['product_id'] : '';
+$product = products_data($product_id);
+$productDiscount = array();
+if ($product[0]['product_id'] != 6) {
+    foreach ($product[0]['price'] as $pkey => $pkvalue) {
+        $pprice = $pkvalue - ($pkvalue * $offers['Welcome'][0]['Offersvalue']) / 100;
+        array_push($productDiscount, $pprice);
+    }
+} else {
+    foreach ($product[0]['pricesqty'] as $pkey => $pkvalue) {
+        foreach ($pkvalue as $pey => $pval) {
+            $pprice = $pval - ($pval * $offers['Welcome'][0]['Offersvalue']) / 100;
+            array_push($productDiscount, $pprice);
+        }
+    }
+}
+$product[0]['discount'] = $productDiscount;
+if (isset($_SESSION['user']['register_id'])) {
+    $user_id = $_SESSION['user']['register_id'];
+    $product_cart = carts_data($user_id);
+    foreach ($product_cart as $key => $value) {
+        $orders = order_data($user_id, $value['cart_id']);
+        if (!empty($orders)) {
+            $product_order[$key] = $orders[0];
+            $product_details = products_data($orders[0]['product_id']);
+            $product_order[$key]['quantity'] = $value['quantity'];
+            $product_order[$key]['price'] = $value['price'];
+            $product_order[$key]['product_name'] = $value['product_name'];
+            $product_order[$key]['products_offers'] = $product_details[0]['offers'];
+        }
+    }
+} else if ((isset($_COOKIE['Guest_cart'])) && ($_COOKIE['Guest_cart'] > 0)) {
+    $order_id = $_COOKIE['Guest_cart'];
+    $orders = order_data('', '', $order_id);
+    if (!empty($orders)) {
+        $product_order[0] = $orders[0];
+        $product_details = products_data($orders[0]['product_id']);
+        if (!empty($product_details[0]['offer_ids'])) {
+            $product_offers = explode(',', $product_details[0]['offers']);
+            $offers_id = explode(',', $product_details[0]['offer_ids']);
+            foreach ($offers_id as $key => $value) {
+                $offer_details = offers_data($value);
+                $product_order[0]['offers'][$key] = $product_offers[$key];
+                $product_order[0]['products_offers'][$key] = $offer_details[0];
+            }
+        }
+    }
+}
+?>
