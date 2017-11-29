873eec745328401a38eb011d52034c1d2d53ca17
racchanak <racchana.appiness@gmail.com>
2017-11-29 18:03:07 +0530

    New Page of Offers
    Signed-off-by: racchanak <racchana.appiness@gmail.com>

---
+++ offers.php
@@ -0,0 +1,54 @@
+<?php
+    include('functioncall.php');
+    $title = 'Offers';
+    include('header.php');
+?>
+<div id="contact-page" class="container">
+    <div class="bg">
+        <div class="row">           
+            <div class="col-sm-12"> 
+                <div class="blog-post-area">
+                    <h2 class="title text-center">Referral Offers</h2>
+                    <div class="single-blog-post">  
+                        <div class="col-sm-4"> 
+                            <div class="col-sm-2"> 
+                                <img src="assets/images/referral/twoperson.png" alt="" class="img-responsive">                              
+                            </div>
+                            <div class="col-sm-2"> 
+                                <img src="assets/images/referral/shareicon.jpg" alt="" class="img-responsive">
+                            </div>
+                        </div>
+                        <div class="col-sm-4"> 
+                            <div class="col-sm-2"> 
+                                <img src="assets/images/referral/singleperson.png" alt="" class="img-responsive">
+                            </div>
+                            <div class="col-sm-2"> 
+                                <img src="assets/images/referral/cartimge.png" alt="" class="img-responsive">
+                            </div>
+                        </div>
+                        <div class="col-sm-4"> 
+                            <div class="col-sm-2">
+                                <img src="assets/images/referral/percentperson.jpg" alt="" class="img-responsive">
+                            </div>
+                        </div> 
+                        <div class="col-sm-12"> 
+                            <p>Encourages their customers to bring in their friends and families to experience the real taste of our traditional items.</p>
+                            <p>Every referral purchase will earn our customers 50 Rs which can be used on their upcoming purchases.</p>  
+                            <p>Five referal purchase would earn flat 250 Rs for our customers.</p>  
+                            <p>A simple three step process mentioned below will provide more clarity on the same,</p>
+                            <ol>
+                                <li>Login to your account at NativeSpecial.com, if not registered already please do register so that you can be part of this referal program.</li>
+                                <li>Click on the link REFERAL BONUS and send the invitation notice to your friends.</li> 
+                                <li>Now your referred friend can register and place order with flat 50Rs discount.</li>  
+                                <li>Once your friend completes his order referrer will get flat 50 Rs added to their account which will get deducted during the checkoutAs always we are looking forward for the support from our customers to make this referal program a success.</li>
+                            </ol> 
+                        </div> 
+                    </div>
+                </div>
+            </div>                  
+        </div>  
+    </div>  
+</div><!--/#contact-page-->
+<?php
+include('footer.php');
+?>
