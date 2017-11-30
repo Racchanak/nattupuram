01167aa3e6a6907599bdd51991e642e675af8926
racchanak <racchana.appiness@gmail.com>
2017-11-30 18:09:41 +0530

    Offers with the referal code

--- offers.php
+++ offers.php
@@ -12,23 +12,19 @@
                     <div class="single-blog-post">  
                         <div class="col-sm-4"> 
                             <div class="col-sm-2"> 
-                                <img src="assets/images/referral/twoperson.png" alt="" class="img-responsive">                              
-                            </div>
-                            <div class="col-sm-2"> 
-                                <img src="assets/images/referral/shareicon.jpg" alt="" class="img-responsive">
+                                <img src="assets/images/referral/twoperson.png" alt="" class="img-responsive">
+                                <img src="assets/images/referral/shareicon.jpg" alt="" class="img-responsive" style="height: 14px;margin-left: 27px;margin-top: -20px;">
                             </div>
                         </div>
                         <div class="col-sm-4"> 
                             <div class="col-sm-2"> 
-                                <img src="assets/images/referral/singleperson.png" alt="" class="img-responsive">
-                            </div>
-                            <div class="col-sm-2"> 
-                                <img src="assets/images/referral/cartimge.png" alt="" class="img-responsive">
+                                <img src="assets/images/referral/singleperson.png" alt="" class="img-responsive" style="height: 20px;">
+                                <img src="assets/images/referral/cartimge.png" alt="" class="img-responsive" style="margin-left: 16px;margin-top: -18px;height: 18px;">
                             </div>
                         </div>
                         <div class="col-sm-4"> 
-                            <div class="col-sm-2">
-                                <img src="assets/images/referral/percentperson.jpg" alt="" class="img-responsive">
+                            <div class="col-sm-3">
+                                <img src="assets/images/referral/percentperson.jpg" alt="" class="img-responsive" style="height: 30px;">
                             </div>
                         </div> 
                         <div class="col-sm-12"> 
